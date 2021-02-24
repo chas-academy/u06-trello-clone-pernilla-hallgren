f5777c9af5bc21459453431b1aad6c4af37d57b8
Pernilla Hällgren <pernilla.hallgren@chasacademy.se>
2021-02-24 12:39:57 +0100

    Add jQuery dialog function

---
+++ script.js
@@ -0,0 +1,87 @@
+$(() => {
+
+    // DIALOG - To-do
+    $('#info-dialogbox-one').dialog({
+        autoOpen: false,
+        title: 'Information - To Do',
+        closeOnEscape: false,
+        show: {
+            effect: 'slide',
+            duration: 500
+        },
+        hide: {
+            effect: 'fade',
+            duration: 400
+        },
+        buttons: {
+            close: function() {$(this).dialog('close');}
+        },
+    });
+    $('.info-1').on('click', function() {
+        $('#info-dialogbox-one').dialog('open');
+    });
+
+    // DIALOG - Working On
+    $('#info-dialogbox-two').dialog({
+        autoOpen: false,
+        title: 'Information - Working On',
+        closeOnEscape: false,
+        show: {
+            effect: 'slide',
+            duration: 500
+        },
+        hide: {
+            effect: 'fade',
+            duration: 400
+        },
+        buttons: {
+            close: function() {$(this).dialog('close');}
+        },
+    });
+    $('.info-2').on('click', function() {
+        $('#info-dialogbox-two').dialog('open');
+    });
+
+    // DIALOG - Fix
+    $('#info-dialogbox-three').dialog({
+        autoOpen: false,
+        title: 'Information - Fix',
+        closeOnEscape: false,
+        show: {
+            effect: 'slide',
+            duration: 500
+        },
+        hide: {
+            effect: 'fade',
+            duration: 400
+        },
+        buttons: {
+            close: function() {$(this).dialog('close');}
+        },
+    });
+    $('.info-3').on('click', function() {
+        $('#info-dialogbox-three').dialog('open');
+    });    
+
+    // DIALOG - Done
+    $('#info-dialogbox-four').dialog({
+        autoOpen: false,
+        title: 'Information - Done',
+        closeOnEscape: false,
+        show: {
+            effect: 'slide',
+            duration: 500
+        },
+        hide: {
+            effect: 'fade',
+            duration: 400
+        },
+        buttons: {
+            close: function() {$(this).dialog('close');}
+        },
+    });
+    $('.info-4').on('click', function() {
+        $('#info-dialogbox-four').dialog('open');
+    });      
+
+});
